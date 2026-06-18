4068c4ac7f29d76d509b632091ac3ca6a4141e8e
d-ansons <davis.ansons@outlook.com>
2026-06-18 21:17:17 +0100

    created init command to create .dgit subdir

--- dgit/cli.py
+++ dgit/cli.py
@@ -1,2 +1,24 @@
+import argparse
+import os
+
+from . import data
+
 def main():
-    print('Hello World!')
+    args = parse_args()
+    args.func(args)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True # dgit requires arguments
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+    init_parser.add_argument('path', nargs='?', default='.') # ? makes argument optional.
+
+    return parser.parse_args()
+
+def init(args):
+    data.init()
+    print(f'Initialised empty dgit repository in {os.getcwd()}/{data.GIT_DIR}')
---
+++ dgit/data.py
@@ -0,0 +1,6 @@
+import os
+
+GIT_DIR = '.dgit'
+
+def init():
+    os.makedirs(GIT_DIR)
